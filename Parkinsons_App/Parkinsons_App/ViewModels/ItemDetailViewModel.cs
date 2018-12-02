2c50450d740629a3e0d9536a3444916155d68e0b
jmlewis1 <jmlewis1@hotmail.com>
2018-12-02 11:32:58 -0800

    First commit

---
+++ Parkinsons_App/Parkinsons_App/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Parkinsons_App.Models;
+
+namespace Parkinsons_App.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
